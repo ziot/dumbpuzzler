47019e5244a4624f174a477c1707d7c03f53a4d4
Brett Buerhaus <hidden@mrzioto.com>
2016-08-12 21:55:39 -0700

    index page, nav changes

--- web/handlers/main.py
+++ web/handlers/main.py
@@ -2,5 +2,7 @@ import tornado.web, sys, json
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
+        self.render(
+            "index.html"
+        )
         return
-        
